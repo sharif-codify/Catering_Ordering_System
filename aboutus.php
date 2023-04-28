b1f1dfc0b918e6dc03b473eb18da5decf8adbbe9
sharif-codify <132066724+sharif-codify@users.noreply.github.com>
2023-04-28 18:09:27 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,137 @@
+<?php
+session_start();
+?>
+
+<html>
+
+  <head>
+    <title> About | only cafe' </title>
+    
+    </head>
+
+  <link rel="stylesheet" type = "text/css" href ="css/aboutus.css">
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+  <body>
+
+  
+    <button onclick="topFunction()" id="myBtn" title="Go to top">
+      <span class="glyphicon glyphicon-chevron-up"></span>
+    </button>
+  
+    <script type="text/javascript">
+      window.onscroll = function()
+      {
+        scrollFunction()
+      };
+
+      function scrollFunction(){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("myBtn").style.display = "block";
+        } else {
+          document.getElementById("myBtn").style.display = "none";
+        }
+      }
+
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    </script>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">only Cafe'</a>
+        </div>
+
+        <div class="collapse navbar-collapse " id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php">Home</a></li>
+            <li class="active"><a href="aboutus.php">About</a></li>
+            <li><a href="contactus.php">Contact Us</a></li>
+          </ul>
+
+<?php
+if(isset($_SESSION['login_user1'])){
+
+?>
+
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user1']; ?> </a></li>
+            <li><a href="myrestaurant.php">MANAGER CONTROL PANEL</a></li>
+            <li><a href="logout_m.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+<?php
+}
+else if (isset($_SESSION['login_user2'])) {
+  ?>
+           <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_user2']; ?> </a></li>
+            <li><a href="foodlist.php"><span class="glyphicon glyphicon-cutlery"></span> Food Zone </a></li>
+            <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart 
+            (<?php
+              if(isset($_SESSION["cart"])){
+              $count = count($_SESSION["cart"]); 
+              echo "$count"; 
+            }
+              else
+                echo "0";
+              ?>)
+            </a></li>
+            <li><a href="logout_u.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+  <?php        
+}
+else {
+
+  ?>
+
+<ul class="nav navbar-nav navbar-right">
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Sign Up <span class="caret"></span> </a>
+                <ul class="dropdown-menu">
+              <li> <a href="customersignup.php"> User Sign-up</a></li>
+              <li> <a href="managersignup.php"> Manager Sign-up</a></li>
+          
+            </ul>
+            </li>
+
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-in"></span> Login <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+              <li> <a href="customerlogin.php"> User Login</a></li>
+              <li> <a href="managerlogin.php"> Manager Login</a></li>
+
+            </ul>
+            </li>
+          </ul>
+
+<?php
+}
+?>
+        </div>
+
+      </div>
+    </nav>
+
+    <div class="wide" style="background-image: url('images/aee.jpeg');height:800px">
+
+        
+        <div class="tagline">It's not our <font color="orange"><strong>work life</strong></font>, it's our <font color="red"><strong><em>life's work</em>.</strong></font></div>
+        <h1  style="color: yellow">About the food culture in only cafe'</h1>
+        <h5 style="color: white">Offer Outdoor Seating</h5>
+
+        <br>
+        <h5 style="color: white">Order food & beverages online from restaurants near & around you. <h5 style="color: white">We deliver food from your neighborhood local joints, your favorite cafes, luxurious & elite restaurants in your area,</h5><h5 style="color :white"> And also from chains like Dominos, KFC, Burger King, Pizza Hut, FreshMenu, Mc Donald's, Subway, Faasos, Cafe Coffee Day, Taco Bell, and more. Exciting bit?</h5><h5 style="color : white"> We place no minimum order restrictions! Order in as little (or as much) as you'd like. We'll Swiggy it to you!!! Only cafe to the youth is a “hangout” spot where they meet people, make conversations, and have a whole lot of fun over steaming cups of great coffee.It's been an exciting journey since then to becoming the largest organised retail cafe chain in the country   Your experience of starting the journey and continuing it with excellence over the years will have to be included in the page -The opening and closing times of the restaurant must be clearly mentioned in as simple a way as possible so that customers can visit accordingly</h5></h5>
+    </div>
+
+         </body>
+</html>
